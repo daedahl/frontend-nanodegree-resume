bb8214eaa158f9bb0bef9a94532ce5a8fd6fa56d
Dave Figueroa <davefig@gmail.com>
2015-08-06 20:30:36 -0400

    feat: Add bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,25 @@ var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var bio = {
+	"name" : "Dave Figueroa",
+	"role" : "Web Developer",
+	"contact" : {
+		"email" : "davefig@gmail.com"
+	},
+	"picture" : "/images/profilepic.png",
+	"welcome" : "Welcome to my resume",
+	"skills" : [
+		"HTML",
+		"CSS",
+		"Javascript"
+	]
+};
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contact.email);
+$("#main").append(bio.picture);
+$("#main").append(bio.welcome);
+$("#main").append(bio.skills);
