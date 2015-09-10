bd81ae804375986abf28ae50b2fcdd66234c3a50
Dave Figueroa <davefig@gmail.com>
2015-09-09 22:30:36 -0400

    refactor: Use display functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,17 +1,11 @@
-var formattedName = HTMLheaderName.replace("%data%", "Dave Figueroa");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
 var bio = {
 	"name" : "Dave Figueroa",
 	"role" : "Web Developer",
 	"contacts" : {
-		"mobile" : "",
+		"mobile" : "abc",
 		"email" : "davefig@gmail.com",
-		"github" : "",
-		"twitter" : "",
+		"github" : "xyz",
+		"twitter" : "123",
 		"location" : "Culpeper, VA"
 	},
 	"welcomeMessage" : "Welcome to my resume",
@@ -20,7 +14,7 @@ var bio = {
 		"CSS",
 		"Javascript"
 	],
-	"biopic" : "/images/profilepic.png",
+	"biopic" : "images/profilepic.png",
 	"display" : ""
 };
 
@@ -77,21 +71,36 @@ var education = {
 	"display" : ""
 };
 
-if (bio.skills) {
-	$("#header").append(HTMLskillsStart);
-	for (each in bio.skills){
+bio.display = function() {
+	$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
+	$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
+	if (bio.contacts) {
+		$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
+		$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
+		$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
+		$("#topContacts").append(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+		$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
+	}
+	$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
+	if (bio.skills) {
+		$("#header").append(HTMLskillsStart);
+		for (each in bio.skills){
 		$("#skills").append(HTMLskills.replace("%data%", bio.skills[each]));
+		}
 	}
 }
 
-if (work.jobs) {
-	for (each in work.jobs){
-		$("#workExperience").append(HTMLworkStart);
-		$(".work-entry:last").append(HTMLworkEmployer.replace("%data%", work.jobs[each].employer));
-		$(".work-entry:last").append(HTMLworkTitle.replace("%data%", work.jobs[each].title));
-		$(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[each].location));
-		$(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[each].dates));
-		$(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[each].description));
+work.display = function() {
+	if (work.jobs) {
+		for (each in work.jobs){
+			$("#workExperience").append(HTMLworkStart);
+			$(".work-entry:last").append(HTMLworkEmployer.replace("%data%", work.jobs[each].employer));
+			$(".work-entry:last").append(HTMLworkTitle.replace("%data%", work.jobs[each].title));
+			$(".work-entry:last").append(HTMLworkLocation.replace("%data%", work.jobs[each].location));
+			$(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[each].dates));
+			$(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[each].description));
+		}
 	}
 }
 
@@ -104,7 +113,33 @@ projects.display = function() {
 	}
 }
 
+education.display = function() {
+	if (education.schools){
+		for (each in education.schools){
+			$("#education").append(HTMLschoolStart);
+			$(".education-entry:last").append(HTMLschoolName.replace("%data%", education.schools[each].name));
+			$(".education-entry:last").append(HTMLschoolDegree.replace("%data%", education.schools[each].degree));
+			$(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[each].dates));
+			$(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[each].location));
+			$(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[each].major));
+		}
+	}
+	if (education.onlineCourses){
+		$("#education").append(HTMLonlineClasses);
+		for (each in education.onlineCourses){
+			$("#education").append(HTMLschoolStart);
+			$(".education-entry:last").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[each].title));
+			$(".education-entry:last").append(HTMLonlineSchool.replace("%data%", education.onlineCourses[each].school));
+			$(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[each].date));
+			$(".education-entry:last").append(HTMLonlineURL.replace("%data%", education.onlineCourses[each].url));
+		}
+	}
+}
+
 projects.display();
+bio.display();
+work.display();
+education.display();
 
 // This logs the location of user screen clicks
 $(document).click(function(loc) {
